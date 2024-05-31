e5f4f56895c01597e48a219925c384190fb92c67
Jmpardo047 <140624378+Jmpardo047@users.noreply.github.com>
2024-05-30 23:22:24 -0500

    proyecto inciado :tada:

---
+++ src/ConfeccionesApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConfeccionesApp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ConfeccionesApp
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ConfeccionesApp from "./ConfeccionesApp";
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <ConfeccionesApp/>
+    </React.StrictMode>
+)
