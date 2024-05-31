974dfbddc0172ad1eaab6027c26a66c04ee6b5fc
Jmpardo047 <140624378+Jmpardo047@users.noreply.github.com>
2024-05-31 13:02:33 -0500

    new components addded :sparkles:

---
+++ src/components/home/BannerComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BannerComponent = () => {
+  return (
+    <div className='banner md:flex'>
+        <h1 className='text-5xl sm:text-7xl font-bold'>Confecciones Pepita</h1>
+    </div>
+  )
+}
+
+export default BannerComponent
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import BannerComponent from '../components/home/BannerComponent'
+import { AddMateriaComponent } from '../components/home/AddMateriaComponent'
 export const HomePage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <BannerComponent/>
+    <AddMateriaComponent/>
+    </>
   )
 }
 
