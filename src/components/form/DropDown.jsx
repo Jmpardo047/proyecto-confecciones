17f4cba6ed09d6170f5c633f061b639610c4bc8b
DannaG18 <158192981+DannaG18@users.noreply.github.com>
2024-05-31 21:55:05 -0500

    css details :lipstick:

--- src/components/form/DropDown.jsx
+++ src/components/form/DropDown.jsx
@@ -40,7 +40,7 @@ export const DropDown = () => {
       <button
         id="multiLevelDropdownButton"
         onClick={toggleDropdown}
-        className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center "
+        className="text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center "
         type="button"
         style={{backgroundColor:'#33DAFF'}}
       >
