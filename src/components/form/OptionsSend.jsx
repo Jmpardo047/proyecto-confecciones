ed84d04caae62bb3664c1c86f5dcfbcf032c5b16
Jmpardo047 <140624378+Jmpardo047@users.noreply.github.com>
2024-06-03 20:12:45 -0500

    calculos iniciados

--- src/components/form/OptionsSend.jsx
+++ src/components/form/OptionsSend.jsx
@@ -14,8 +14,8 @@ export const OptionsSend = () => {
         number_empleados: "",
         salario_base: "",
         salario_hora: "",
-        materia_prima: {}
-
+        materia_prima: {},
+        costo_final:""
     })
 
     const handleChange = (event) =>{
@@ -25,8 +25,17 @@ export const OptionsSend = () => {
             [name]:value
         })
     }
+    const calcFinalCost = ({cantidad_producto,number_empleados,salario_base,salario_hora}) =>{
+        let beneficios = 200;
+        let costosIndirectos = 50;
+        let salarioFinal = (parseInt(salario_base)*parseInt(salario_hora));
+        let costoTrabajadores = (parseInt(number_empleados)*parseInt(salarioFinal))+(parseInt(number_empleados)*costosIndirectos)+(parseInt(number_empleados)*beneficios);
+
+        console.log(costoTrabajadores)
+    }
     const handleSubmit = (event) =>{
         event.preventDefault();
+        calcFinalCost(formLote);
         postForm(formLote)
     }
 
@@ -36,7 +45,7 @@ export const OptionsSend = () => {
         <div className='main-header banner-form '>
             <h1 className='main-title text-5xl sm:text-7xl font-bold'>Crea tu lote</h1>
         </div>
-        <form className="flex flex-col items-center justify-around  h-fit w-4/5  sm:h-3/5 mt-5 rounded-lg p-5 mx-auto sm:w-3/5"  style={{backgroundColor:"black"}} onSubmit={handleSubmit}>
+        <form className="flex flex-col items-center justify-around  h-fit w-4/5  sm:h-3/5 mt-5 rounded-lg p-5 mx-auto sm:w-3/5 mb-4"  style={{backgroundColor:"black"}} onSubmit={handleSubmit}>
           <div className="text-center text-white">
             <p className="font-bold ">Client Form</p>
             <p>Ingresa los datos para hacer tu cotización personalizada</p>
@@ -52,21 +61,21 @@ export const OptionsSend = () => {
             </div>
             <div>
             <label htmlFor="salario_base" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Salario Base</label>
-                <input name="salario_base" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white" code="salario_base" onChange={handleChange} required />
+                <input name="salario_base" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white" code="salario_base" onChange={handleChange} required />
             </div>
             <div>
             <label htmlFor="cantidad_producto" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad producto</label> 
-                <input name="cantidad_producto" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white"  code="cantidad_producto" onChange={handleChange} required />
+                <input name="cantidad_producto" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white"  code="cantidad_producto" onChange={handleChange} required />
             </div>
             <div>
             <label htmlFor="salario_hora" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Salario Hora</label> 
-                <input name="salario_hora" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white" code="salario_hora" required onChange={handleChange} />
+                <input name="salario_hora" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white" code="salario_hora" required onChange={handleChange} />
             </div>
             <div>
                 <DropDown name="materia_prima" code="materia_prima" value={formLote.materia_prima} onChange={handleChange} />
             </div>
         </div>
-        <input type="submit" className=" size-1/5 h-12 bg-violet-700 text-white p-2 rounded hover:bg-violet-900 cursor-pointer" required/>
+        <input type="submit" className=" size-1/5 h-12 bg-violet-700 text-white p-2 rounded hover:bg-violet-900 cursor-pointer mt-5" required/>
         </form>
     </div>
 
